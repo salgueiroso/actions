dea7ccff8fd0065dcbbe18c066482d672207403f
Acacio Salgueiro <acacioxp@gmail.com>
2024-08-22 10:54:35 -0300

    teste

--- acts/scripts/requirements_validate.sh
+++ acts/scripts/requirements_validate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-echo $GITHUB_WORKSPACE
+
+# Este trecho verifica se a geração do arquivo packages.lock.json está habilitada em algum dos projetos.
+# A finalidade deste requisito serve para que o dotnet possa fazer cache local dos pacotes nuget
 grep -rnwiq --include=\*.csproj "<RestorePackagesWithLockFile>true</RestorePackagesWithLockFile>" "${GITHUB_WORKSPACE}" || { echo "Nenhum projeto (csproj) contém a propriedade RestorePackagesWithLockFile. Veirifique a documentacao online https://devblogs.microsoft.com/nuget/enable-repeatable-package-restores-using-a-lock-file/"; exit 1; }
 
 
