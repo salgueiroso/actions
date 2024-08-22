72cd62b76cd48c2fd72e79cfbd5c72a769be8d1b
Acacio Salgueiro <acacioxp@gmail.com>
2024-08-22 09:49:55 -0300

    script

--- acts/scripts/requirements_validate.sh
+++ acts/scripts/requirements_validate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-grep -qrw --include="*.csproj" "<RestorePackagesWithLockFile>true</RestorePackagesWithLockFile>" "${GITHUB_WORKSPACE}" || { echo "Nenhum projeto (csproj) contém a propriedade RestorePackagesWithLockFile. Veirifique a documentacao online https://devblogs.microsoft.com/nuget/enable-repeatable-package-restores-using-a-lock-file/"; exit 1; }
+echo $GITHUB_WORKSPACE
+grep -rnwiq --include=\*.csproj "<RestorePackagesWithLockFile>true</RestorePackagesWithLockFile>" "${GITHUB_WORKSPACE}" || { echo "Nenhum projeto (csproj) contém a propriedade RestorePackagesWithLockFile. Veirifique a documentacao online https://devblogs.microsoft.com/nuget/enable-repeatable-package-restores-using-a-lock-file/"; exit 1; }
 
 
 echo Validacao de requisitos de projeto concluida!
