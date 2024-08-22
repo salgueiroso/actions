e1d6c46994fcf827bd2e1b52d5545704cc9b0c39
Acacio Salgueiro <acacioxp@gmail.com>
2024-08-22 09:30:19 -0300

    script

--- acts/scripts/requirements_validate.sh
+++ acts/scripts/requirements_validate.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+printenv
+
+grep -qrw --include="*.csproj" "RestorePackagesWithLockFile" "caminho/para/sua/pasta" || { echo "Nenhum arquivo contém a palavra especificada"; exit 1; }
+
+
 echo Validado!
